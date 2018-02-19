7104e4c520b67e26d1354a593b42068c702eecf0
Rohitha1990 <kothmeerrohitha@gmail.com>
2018-02-19 12:43:11 +0530

    Assignments

---
+++ AssignmentArrays/src/praticse1/Example2array.java
@@ -0,0 +1,10 @@
+package praticse1;
+
+public class Example2array {
+
+	public static void main(String[] args) {
+		int a[]= {10,20,30,40,55};
+		for(int i=0;i<a.length;i++)
+		System.out.println(a[i]);
+	}
+}
---
+++ AssignmentArrays/src/praticse1/Example4array.java
@@ -0,0 +1,19 @@
+package praticse1;
+
+public class Example4array {
+	static void max(int arr[]) {
+		int max=arr[0];
+		for(int i=1;i>arr.length;i--)
+		if(max<arr[i])
+			max=arr[i];
+			System.out.println(max);
+	}
+
+	public static void main(String[] args) {
+		int a[]= {33,78,0,1};
+		max(a);
+		
+
+	}
+
+}
